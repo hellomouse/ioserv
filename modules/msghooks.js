b059c4f00027b31e8dc3575f80b4e09a92666f1f
hellomouse <hellomouse@hellomouse.net>
2021-06-12 05:17:52 +0000

    fix msghooks error
    Co-Authored-By: iovoid <iovoid4@gmail.com>
    Co-Authored-By: iczero <iczero4@gmail.com>

--- modules/msghooks.js
+++ modules/msghooks.js
@@ -1,5 +1,6 @@
 module.exports = function load(bot) {
   bot.events.on('privmsg', event => {
+    if (event.type !== 'msg') return;
     if (event.args[0].toLowerCase() === '!chaos') event.reply('y u do dis');
   });
 };
